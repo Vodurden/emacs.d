266a49ed52e55a0a0c31aa8f895c650f768eb9c4
Jake Woods <vodurden@gmail.com>
2014-11-04 14:14:56 +1100

    Added navigate symbols and company mode support for csharp

--- config/init-csharp.el
+++ config/init-csharp.el
@@ -3,30 +3,18 @@
 
 ; omnisharp-emacs
 ;
-; IMPORTANT: Make sure omnisharp server is installed on the executing machine. 
+; IMPORTANT: Make sure omnisharp server is installed on the executing machine.
 ;            Without it omnisharp-emacs will not work.
 ;
 (require-package 'omnisharp)
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
 
+;; Company mode integration to omnisharp
+(add-hook 'csharp-mode-hook 'company-mode) ; Use company mode when in a haskell file
+(add-to-list 'company-backends 'company-omnisharp)
+
+; Keybindings
+(evil-leader/set-key
+  "ns" 'omnisharp-helm-find-symbols) ; Navigate symbols
+
 (provide 'init-csharp)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
